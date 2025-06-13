eb2278c83975383c6efcaa6c1e85dcea4f45a624
Piotrek Budzowski <piobudzows@gmail.com>
2025-06-13 19:40:46 +0200

    Update BDConector.py

--- Clases/BDConector.py
+++ Clases/BDConector.py
@@ -25,6 +25,7 @@ class BDConector:
             print("Conexión a la base de datos cerrada correctamente.")
         
 
+
     # INTENTA INICIAR SESIÓN VERIFICANDO LAS CREDENCIALES DEL USUARIO.
     # MANEJA LA CONEXIÓN Y DESCONEXIÓN A LA BASE DE DATOS INTERNAMENTE.
     # RETORNA EL RESULTADO DE LA CONSULTA (FILA DE USUARIO) SI ES EXITOSO,
@@ -50,5 +51,42 @@ class BDConector:
         except mysql.connector.Error as err:
             print(f"Error al verificar credenciales: {err}")
             return None
+        finally:
+            self.desconectar() # Asegura que la conexión se cierre al finalizar
+
+
+    # Este método intenta registrar un nuevo usuario, verificando primero si el nombre de usuario ya existe. 
+    # Maneja la conexión y desconexión a la base de datos internamente. 
+    # Retorna True si el registro es exitoso, 'usuario_existente' 
+    # si el nombre de usuario ya está en uso, False si hay un error de base de datos,
+    # o 'error_conexion' si no se pudo conectar.
+    def registrar_usuario_bd(self, nombre_usuario, contraseña, correo):
+        self.conectar() # Intenta conectar a la base de datos
+        
+        if not self.conexion or not self.conexion.is_connected():
+            print("Error: No se pudo establecer conexión a la base de datos para el registro.")
+            return 'error_conexion'
+
+        try:
+            cursor = self.conexion.cursor()
+            # CONSULTAR SI EL USUARIO YA EXISTE
+            cursor.execute("SELECT * FROM usuario WHERE nombre_usuario = %s", (nombre_usuario,))
+            usuario_existente = cursor.fetchone()
+            
+            if usuario_existente:
+                cursor.close()
+                return 'usuario_existente'
+            
+            # INSERTAR NUEVO USUARIO CON tipo = 'Usuario'
+            cursor.execute("INSERT INTO usuario (nombre_usuario, contraseña, correo, tipo) VALUES (%s, %s, %s, %s)",
+                (nombre_usuario, contraseña, correo, 'Usuario')
+            )
+            self.conexion.commit()   # PARA GUARDAR LOS CAMBIOS EN LA BASE DE DATOS
+            cursor.close()
+            return True
+            
+        except mysql.connector.Error as err:
+            print(f"Error al registrar usuario: {err}")
+            return False
         finally:
             self.desconectar() # Asegura que la conexión se cierre al finalizar
